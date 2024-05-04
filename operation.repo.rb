7e531102e117c26fa1a46e51d79e251aa2d8b82c
elprofesor <rrazvan.radeanu@gmail.com>
2024-05-04 18:43:46 +0300

    Revert "changed url for tqdm dependency"
    This reverts commit 8b88b0325526d31cb03e200a0e31a6a77e81da4f.

--- operation.repo.rb
+++ operation.repo.rb
@@ -11,8 +11,8 @@ class OperationRepo < Formula
 
 
   resource "tqdm" do
-    url "https://github.com/tqdm/tqdm/archive/refs/tags/v4.66.4.tar.gz"
-    sha256 "ffaf75fceb3d7862cf3eb0a1dc2688641cb09fb2b44394d49d7977122785106c"
+    url "https://files.pythonhosted.org/packages/ea/85/3ce0f9f7d3f596e7ea57f4e5ce8c18cb44e4a9daa58ddb46ee0d13d6bff8/tqdm-4.66.4.tar.gz"
+    sha256 "6c3ce127a8e4642c93eb96eb46bd1c0f9f88dac03fc7f08d00d7ffc1d2e8c1c8"
   end
 
 
