464107d1c2968a1f9bbe155ad22f10c6ad1c3043
elprofesor <rrazvan.radeanu@gmail.com>
2026-02-05 02:15:55 +0200

    add operation.repo version 3.0.0

--- operation.repo.rb
+++ operation.repo.rb
@@ -1,22 +1,20 @@
 class OperationRepo < Formula
   include Language::Python::Virtualenv
 
-  desc "Creates an operation folder repo like Git commandline."
+  desc "Git-like CLI tool for organizing operations, pentests, and projects"
   homepage "https://github.com/elprofesor96/operation.repo"
-  url "https://github.com/elprofesor96/operation.repo/archive/refs/tags/v2.1.1.tar.gz"
-  sha256 "cc93c45ef5692cc84de51d832cb3fbc3678528294d9ffff99d8e5771666f7044"
+  url "https://github.com/elprofesor96/operation.repo/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "25d4fbfee3f5a8b97f737af7b6d711b4349b4ccc19ae398a21816eaa328959de"
   license "MIT"
 
   depends_on "python@3.12"
 
   def install
     virtualenv_install_with_resources
-
-   # (etc/"operation.repo").mkpath
-   # etc.install "op.conf" => "operation.repo/op.conf"
   end
 
   test do
-   
+    assert_match "op version", shell_output("#{bin}/op --version")
+    assert_match "Usage", shell_output("#{bin}/op --help")
   end
 end
