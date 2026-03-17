7b02b0c87f8a58272dce7e254ad6a9c9517d96e2
elprofesor <rrazvan.radeanu@gmail.com>
2026-03-17 22:54:23 +0200

    add bcrypt for op repo

--- operation.repo.rb
+++ operation.repo.rb
@@ -151,6 +151,11 @@ class OperationRepo < Formula
     sha256 "600f49d217304a5902ac3c37e1281c9fe94e4d0489de643a9504c5cdfdfc6b29"
   end
 
+  resource "bcrypt" do
+    url "https://files.pythonhosted.org/packages/d4/36/3329e2518d70ad8e2e5817d5a4cac6bba05a47767ec416c7d020a965f408/bcrypt-5.0.0.tar.gz"
+    sha256 "f748f7c2d6fd375cc93d3fba7ef4a9e3a092421b8dbf34d8d4dc06be9492dfdd"
+  end
+
   test do
     assert_match "op version", shell_output("#{bin}/op --version")
     assert_match "Usage", shell_output("#{bin}/op --help")
