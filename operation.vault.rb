5f84350abfe7da4acebaecb327809cb1cf506788
elprofesor <rrazvan.radeanu@gmail.com>
2026-03-23 23:37:32 +0200

    add initial opvault v2

---
+++ operation.vault.rb
@@ -0,0 +1,66 @@
+class OperationVault < Formula
+    include Language::Python::Virtualenv
+  
+    desc "Secure credential storage for security engagements."
+    homepage "https://github.com/elprofesor96/operation.vault"
+    url "https://github.com/elprofesor96/operation.vault/releases/tag/v2.0.0"
+    sha256 "bdcc7b02c5cd48de4ff984be50229009a931cc66a0af94ef94e928ec96c52a6c"
+    license "CC-BY-NC-ND-4.0"
+  
+    depends_on "python@3.12"
+  
+    def install
+      virtualenv_install_with_resources
+    end
+
+
+    resource "cffi" do
+        url "https://files.pythonhosted.org/packages/eb/56/b1ba7935a17738ae8453301356628e8147c79dbb825bcbc73dc7401f9846/cffi-2.0.0.tar.gz"
+        sha256 "44d1b5909021139fe36001ae048dbdde8214afa20200eda0f64c068cac5d5529"
+    end
+
+    resource "click" do
+        url "https://files.pythonhosted.org/packages/3d/fa/656b739db8587d7b5dfa22e22ed02566950fbfbcdc20311993483657a5c0/click-8.3.1.tar.gz"
+        sha256 "12ff4785d337a1bb490bb7e9c2b1ee5da3112e94a8622f26a6c77f5d2fc6842a"
+    end
+
+    resource "cryptography" do
+        url "https://files.pythonhosted.org/packages/60/04/ee2a9e8542e4fa2773b81771ff8349ff19cdd56b7258a0cc442639052edb/cryptography-46.0.5.tar.gz"
+        sha256 "abace499247268e3757271b2f1e244b36b06f8515cf27c4d49468fc9eb16e93d"
+    end
+
+    resource "markdown-it-py" do
+        url "https://files.pythonhosted.org/packages/5b/f5/4ec618ed16cc4f8fb3b701563655a69816155e79e24a17b651541804721d/markdown_it_py-4.0.0.tar.gz"
+        sha256 "cb0a2b4aa34f932c007117b194e945bd74e0ec24133ceb5bac59009cda1cb9f3"
+    end
+
+    resource "mdurl" do
+        url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+        sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+    end
+
+    resource "pycparser" do
+        url "https://files.pythonhosted.org/packages/1b/7d/92392ff7815c21062bea51aa7b87d45576f649f16458d78b7cf94b9ab2e6/pycparser-3.0.tar.gz"
+        sha256 "600f49d217304a5902ac3c37e1281c9fe94e4d0489de643a9504c5cdfdfc6b29"
+    end
+
+    resource "Pygments" do
+        url "https://files.pythonhosted.org/packages/b0/77/a5b8c569bf593b0140bde72ea885a803b82086995367bf2037de0159d924/pygments-2.19.2.tar.gz"
+        sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
+    end
+
+    resource "rich-click" do
+        url "https://files.pythonhosted.org/packages/04/27/091e140ea834272188e63f8dd6faac1f5c687582b687197b3e0ec3c78ebf/rich_click-1.9.7.tar.gz"
+        sha256 "022997c1e30731995bdbc8ec2f82819340d42543237f033a003c7b1f843fc5dc"
+    end
+
+    resource "rich" do
+        url "https://files.pythonhosted.org/packages/b3/c6/f3b320c27991c46f43ee9d856302c70dc2d0fb2dba4842ff739d5f46b393/rich-14.3.3.tar.gz"
+        sha256 "b8daa0b9e4eef54dd8cf7c86c03713f53241884e814f4e2f5fb342fe520f639b"
+    end
+
+    test do
+        assert_match "opvault version", shell_output("#{bin}/opvault --version")
+        assert_match "Usage", shell_output("#{bin}/opvault --help")
+      end
+    end
